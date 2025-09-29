80baa6c2b3b90506f340c020b8ed4590ee8abe3e
Tekizaki <dzakitaufiqullah@gmail.com>
2025-09-29 11:27:37 +0700

    Update repository

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-// app.js
 const Fastify = require("fastify");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
-const lspRoutes = require("./routes/lspRoutes"); // Import rute LSP
+const lspRoutes = require("./routes/lspRoutes");
+const cors = require("@fastify/cors");
 
 function buildApp(opts = {}) {
   const fastify = Fastify(opts);
 
+  // Register CORS - *Pastikan ini di awal sebelum rute lain*
+  fastify.register(cors, {
+    origin: "*", // Ganti dengan 'http://localhost:8080' atau domain frontend Anda di produksi
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Tambahkan PATCH jika digunakan
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
+
   // Register routes
-  fastify.register(authRoutes, { prefix: "/api/auth" }); // Ganti prefix ke /api/auth
-  fastify.register(userRoutes, { prefix: "/api/users" }); // Ganti prefix ke /api/users
-  fastify.register(lspRoutes, { prefix: "/api/lsps" }); // Register rute LSP dengan prefix /api/lsps
+  fastify.register(authRoutes, { prefix: "/api/auth" });
+  fastify.register(userRoutes, { prefix: "/api/users" });
+  fastify.register(lspRoutes, { prefix: "/api/lsps" });
 
   fastify.get("/", async (request, reply) => {
     return { message: "Welcome to LSP Backend API!" };
